a86c54d4cf8aa6f98c3bea2a154075b063b2384d
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-01-23 11:36:57 -0300

    refactor: ajuste na organizacao do codigo

--- assets/main.js
+++ assets/main.js
@@ -6,7 +6,7 @@ fetch(endpoint)
     .then((response) => response.json()) // busca os dados
     .then((data) => cities.push(...data)); // insire os dados no array
 
-function findMatches(wordToMatch, cities) {
+function filterCity(wordToMatch, cities) {
     return cities.filter((place) => {
         // filtrar as palavras que dão match com o que for pesquisado
         const regex = new RegExp(wordToMatch, "gi");
@@ -15,9 +15,9 @@ function findMatches(wordToMatch, cities) {
 }
 
 
-function displayMatches() {
-    const matchAray = findMatches(this.value, cities);
-    const htmlCreator = matchAray
+function showCities() { // Add no html as cidades
+    const filterAray = filterCity(this.value, cities);
+    const htmlCreator = filterAray
         .map((place) => {
             const regex = new RegExp(this.value, "gi");
             const cityName = place.nome.replace(
@@ -35,8 +35,11 @@ function displayMatches() {
     suggestions.innerHTML = htmlCreator;
 }
 
+
+// Parte principal
+
 const searchInput = document.querySelector(".search");
 const suggestions = document.querySelector(".suggestions");
 
-searchInput.addEventListener("change", displayMatches);
-searchInput.addEventListener("keyup", displayMatches);
+searchInput.addEventListener("change", showCities);
+searchInput.addEventListener("keyup", showCities);
